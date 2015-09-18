e87cacf1e0ed1fe44e1119e86b36376b2dac8eb4
Brian Tanabe <btanabe2@gmail.com>
2015-09-17 20:05:27 -0700

    Got rid of unused variables and cleaned up whitespace.

--- src/test/java/com/btanabe/fsdu/test/integration/web/WebRequestTests.java
+++ src/test/java/com/btanabe/fsdu/test/integration/web/WebRequestTests.java
@@ -3,7 +3,6 @@ package com.btanabe.fsdu.test.integration.web;
 import com.btanabe.fsdu.web.WebRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -17,15 +16,10 @@ import static org.junit.Assert.assertThat;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class WebRequestTests {
 
-    @Autowired
-    private String espnProjectionsPageOne;
-
     @Test
     public void shouldBeAbleToDownloadNumberFireSource() throws Exception {
         WebRequest webRequest = new WebRequest();
         String htmlFromFunction = webRequest.downloadWebPageSource("http://games.espn.go.com/ffl/tools/projections?");
         assertThat(htmlFromFunction, containsString("<title>2015 Fantasy Football Rankings &amp; Projections - Free Fantasy Football - ESPN</title>"));
     }
-
-
 }
