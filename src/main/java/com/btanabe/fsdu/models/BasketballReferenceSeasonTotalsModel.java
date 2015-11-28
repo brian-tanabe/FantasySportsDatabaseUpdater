18d5e5ec64f02254e50b09fbc0afe45125134251
Brian Tanabe <btanabe2@gmail.com>
2015-11-27 21:13:12 -0800

    Implemented the ValueExtractors necessary to populate the player models from the basketball-reference season totals pages.

---
+++ src/main/java/com/btanabe/fsdu/models/BasketballReferenceSeasonTotalsModel.java
@@ -0,0 +1,255 @@
+package com.btanabe.fsdu.models;
+
+/**
+ * Created by Brian on 11/22/15.
+ */
+public class BasketballReferenceSeasonTotalsModel {
+    private String name;
+    private String team;
+    private Integer gamesStarted;
+    private Integer gamesPlayed;
+    private Integer minutesPlayed;
+    private Integer fieldGoalsMade;
+    private Integer fieldGoalsAttempted;
+    private Double fieldGoalPercentage;
+    private Integer threePointersMade;
+    private Integer threePointersAttempted;
+    private Double threePointPercentage;
+    private Integer twoPointersMade;
+    private Integer twoPointersAttempted;
+    private Double twoPointPercentage;
+    private Double effectiveFieldGoalPercentage;
+    private Integer freeThrowsMade;
+    private Integer freeThrowsAttempted;
+    private Double freeThrowPercentage;
+    private Integer offensiveRebounds;
+    private Integer defensiveRebounds;
+    private Integer totalRebounds;
+    private Integer assists;
+    private Integer steals;
+    private Integer blocks;
+    private Integer turnovers;
+    private Integer points;
+    private Integer personalFouls;
+
+    @Override
+    public String toString() {
+        return String.format("name=[%s], team=[%s], gamesStarted=[%d], gamesPlayed=[%d], minutesPlayed=[%d], fieldGoalsMade=[%d], fieldGoalsAttempted=[%d], fieldGoalPercentage=[%d], threePointersMade=[%d], threePointersAttempted=[%d], threePointPercentage=[%d], twoPointersMade=[%d], twoPointersAttempted=[%d], twoPointPercentage=[%d], effectiveFieldGoalPercentage=[%d], freeThrowsMade=[%d], freeThrowsAttempted=[%d], freeThrowPercentage=[%d], offensiveRebounds=[%d], defensiveRebounds=[%d], totalRebounds=[%d], assists=[%d], steals=[%d], blocks=[%d], turnovers=[%d], points=[%d], personalFouls=[%d]", name, team, gamesStarted, gamesPlayed, minutesPlayed, fieldGoalsMade, fieldGoalsAttempted, fieldGoalPercentage, threePointersMade, threePointersAttempted, threePointPercentage, twoPointersMade, twoPointersAttempted, twoPointPercentage, effectiveFieldGoalPercentage, freeThrowsMade, freeThrowsAttempted, freeThrowPercentage, offensiveRebounds, defensiveRebounds, totalRebounds, assists, steals, blocks, turnovers, points, personalFouls);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public Integer getGamesStarted() {
+        return gamesStarted;
+    }
+
+    public void setGamesStarted(Integer gamesStarted) {
+        this.gamesStarted = gamesStarted;
+    }
+
+    public Integer getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public void setGamesPlayed(Integer gamesPlayed) {
+        this.gamesPlayed = gamesPlayed;
+    }
+
+    public Integer getMinutesPlayed() {
+        return minutesPlayed;
+    }
+
+    public void setMinutesPlayed(Integer minutesPlayed) {
+        this.minutesPlayed = minutesPlayed;
+    }
+
+    public Integer getFieldGoalsAttempted() {
+        return fieldGoalsAttempted;
+    }
+
+    public void setFieldGoalsAttempted(Integer fieldGoalsAttempted) {
+        this.fieldGoalsAttempted = fieldGoalsAttempted;
+    }
+
+    public Integer getFieldGoalsMade() {
+        return fieldGoalsMade;
+    }
+
+    public void setFieldGoalsMade(Integer fieldGoalsMade) {
+        this.fieldGoalsMade = fieldGoalsMade;
+    }
+
+    public Double getFieldGoalPercentage() {
+        return fieldGoalPercentage;
+    }
+
+    public void setFieldGoalPercentage(Double fieldGoalPercentage) {
+        this.fieldGoalPercentage = fieldGoalPercentage;
+    }
+
+    public Integer getThreePointersMade() {
+        return threePointersMade;
+    }
+
+    public void setThreePointersMade(Integer threePointersMade) {
+        this.threePointersMade = threePointersMade;
+    }
+
+    public Integer getThreePointersAttempted() {
+        return threePointersAttempted;
+    }
+
+    public void setThreePointersAttempted(Integer threePointersAttempted) {
+        this.threePointersAttempted = threePointersAttempted;
+    }
+
+    public Double getThreePointPercentage() {
+        return threePointPercentage;
+    }
+
+    public void setThreePointPercentage(Double threePointPercentage) {
+        this.threePointPercentage = threePointPercentage;
+    }
+
+    public Integer getTwoPointersMade() {
+        return twoPointersMade;
+    }
+
+    public void setTwoPointersMade(Integer twoPointersMade) {
+        this.twoPointersMade = twoPointersMade;
+    }
+
+    public Integer getTwoPointersAttempted() {
+        return twoPointersAttempted;
+    }
+
+    public void setTwoPointersAttempted(Integer twoPointersAttempted) {
+        this.twoPointersAttempted = twoPointersAttempted;
+    }
+
+    public Double getTwoPointPercentage() {
+        return twoPointPercentage;
+    }
+
+    public void setTwoPointPercentage(Double twoPointPercentage) {
+        this.twoPointPercentage = twoPointPercentage;
+    }
+
+    public Double getEffectiveFieldGoalPercentage() {
+        return effectiveFieldGoalPercentage;
+    }
+
+    public void setEffectiveFieldGoalPercentage(Double effectiveFieldGoalPercentage) {
+        this.effectiveFieldGoalPercentage = effectiveFieldGoalPercentage;
+    }
+
+    public Integer getFreeThrowsMade() {
+        return freeThrowsMade;
+    }
+
+    public void setFreeThrowsMade(Integer freeThrowsMade) {
+        this.freeThrowsMade = freeThrowsMade;
+    }
+
+    public Integer getFreeThrowsAttempted() {
+        return freeThrowsAttempted;
+    }
+
+    public void setFreeThrowsAttempted(Integer freeThrowsAttempted) {
+        this.freeThrowsAttempted = freeThrowsAttempted;
+    }
+
+    public Double getFreeThrowPercentage() {
+        return freeThrowPercentage;
+    }
+
+    public void setFreeThrowPercentage(Double freeThrowPercentage) {
+        this.freeThrowPercentage = freeThrowPercentage;
+    }
+
+    public Integer getOffensiveRebounds() {
+        return offensiveRebounds;
+    }
+
+    public void setOffensiveRebounds(Integer offensiveRebounds) {
+        this.offensiveRebounds = offensiveRebounds;
+    }
+
+    public Integer getDefensiveRebounds() {
+        return defensiveRebounds;
+    }
+
+    public void setDefensiveRebounds(Integer defensiveRebounds) {
+        this.defensiveRebounds = defensiveRebounds;
+    }
+
+    public Integer getTotalRebounds() {
+        return totalRebounds;
+    }
+
+    public void setTotalRebounds(Integer totalRebounds) {
+        this.totalRebounds = totalRebounds;
+    }
+
+    public Integer getAssists() {
+        return assists;
+    }
+
+    public void setAssists(Integer assists) {
+        this.assists = assists;
+    }
+
+    public Integer getSteals() {
+        return steals;
+    }
+
+    public void setSteals(Integer steals) {
+        this.steals = steals;
+    }
+
+    public Integer getBlocks() {
+        return blocks;
+    }
+
+    public void setBlocks(Integer blocks) {
+        this.blocks = blocks;
+    }
+
+    public Integer getTurnovers() {
+        return turnovers;
+    }
+
+    public void setTurnovers(Integer turnovers) {
+        this.turnovers = turnovers;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+    public Integer getPersonalFouls() {
+        return personalFouls;
+    }
+
+    public void setPersonalFouls(Integer personalFouls) {
+        this.personalFouls = personalFouls;
+    }
+}
--- src/test/java/com/btanabe/fsdu/test/unit/parsers/value/extractors/BasketballReferenceSeasonTotalsValueExtractorTests.java
+++ src/test/java/com/btanabe/fsdu/test/unit/parsers/value/extractors/BasketballReferenceSeasonTotalsValueExtractorTests.java
@@ -1,11 +1,17 @@
 package com.btanabe.fsdu.test.unit.parsers.value.extractors;
 
+import com.btanabe.fsdu.models.BasketballReferenceSeasonTotalsModel;
+import com.btanabe.fsdu.parsers.ValueExtractor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by Brian on 11/22/15.
@@ -18,132 +24,278 @@ public class BasketballReferenceSeasonTotalsValueExtractorTests {
     @Qualifier("basketballReferenceSeasonTotals20142015QuincyAcy")
     private String quincyAcySeasonTotals;
 
-    @Test
-    public void shouldBeAbleToExtractFullNameFromSeasonsTotalsPage() {
+    @Autowired
+    @Qualifier("qunicyAcy20142015SeasonTotalsPlayerModel")
+    private BasketballReferenceSeasonTotalsModel expectedQuincyAcyModel;
 
+    @Autowired
+    @Qualifier("basketballReferenceNameValueExtractor")
+    private ValueExtractor nameValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceTeamNameValueExtractor")
+    private ValueExtractor teamNameValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceGamesPlayedValueExtractor")
+    private ValueExtractor gamesPlayedValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceGamesStartedValueExtractor")
+    private ValueExtractor gamesStartedValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceMinutesPlayedValueExtractor")
+    private ValueExtractor minutesPlayedValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceFieldGoalsAttemptedValueExtractor")
+    private ValueExtractor fieldGoalsAttemptedValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceFieldGoalsMadeValueExtractor")
+    private ValueExtractor fieldGoalsMadeValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceFieldGoalPercentageValueExtractor")
+    private ValueExtractor fieldGoalPercentageValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceThreePointersAttemptedValueExtractor")
+    private ValueExtractor threePointersAttemptedValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceThreePointersMadeValueExtractor")
+    private ValueExtractor threePointersMadeValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceThreePointPercentageValueExtractor")
+    private ValueExtractor threePointPercentageValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceTwoPointersAttemptedValueExtractor")
+    private ValueExtractor twoPointersAttemptedValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceTwoPointersMadeValueExtractor")
+    private ValueExtractor twoPointersMadeValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceTwoPointersPercentageValueExtractor")
+    private ValueExtractor twoPointerPercentageValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceEffectiveFieldGoalPercentageValueExtractor")
+    private ValueExtractor effectiveFieldGoalPercentageValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceFreeThrowsAttemptedValueExtractor")
+    private ValueExtractor freeThrowsAttemptedValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceFreeThrowsMadeValueExtractor")
+    private ValueExtractor freeThrowsMadeValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceFreeThrowPercentageValueExtractor")
+    private ValueExtractor freeThrowsPercentageValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceOffensiveReboundsValueExtractor")
+    private ValueExtractor offensiveReboundsValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceDefensiveReboundsValueExtractor")
+    private ValueExtractor defensiveReboundsValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceTotalReboundsValueExtractor")
+    private ValueExtractor totalReboundsValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceAssistsValueExtractor")
+    private ValueExtractor assistsValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceStealsValueExtractor")
+    private ValueExtractor stealsValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceBlocksValueExtractor")
+    private ValueExtractor blocksValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferenceTurnoversValueExtractor")
+    private ValueExtractor turnoversValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferencePersonalFoulsValueExtractor")
+    private ValueExtractor personalFoulsValueExtractor;
+
+    @Autowired
+    @Qualifier("basketballReferencePointsValueExtractor")
+    private ValueExtractor pointsValueExtractor;
+
+
+    @Test
+    public void shouldBeAbleToExtractFullNameFromSeasonsTotalsPage() throws Exception {
+        nameValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(nameValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getName())));
     }
 
     @Test
-    public void shouldBeAbleToExtractGamesPlayedFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractTeamAbbreviationFromSeasonTotalsPage() throws Exception {
+        teamNameValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(teamNameValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getTeam())));
     }
 
     @Test
-    public void shouldBeAbleToExtractGamesStartedFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractGamesPlayedFromSeasonsTotalsPage() throws Exception {
+        gamesPlayedValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(gamesPlayedValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getGamesPlayed())));
     }
 
     @Test
-    public void shouldBeAbleToExtractMinutesPlayedFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractGamesStartedFromSeasonsTotalsPage() throws Exception {
+        gamesStartedValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(gamesStartedValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getGamesStarted())));
     }
 
     @Test
-    public void shouldBeAbleToExtractFieldGoalsAttemptedFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractMinutesPlayedFromSeasonsTotalsPage() throws Exception {
+        minutesPlayedValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(minutesPlayedValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getMinutesPlayed())));
     }
 
     @Test
-    public void shouldBeAbleToExtractFieldGoalsMadeFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractFieldGoalsAttemptedFromSeasonsTotalsPage() throws Exception {
+        fieldGoalsAttemptedValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(fieldGoalsAttemptedValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getFieldGoalsAttempted())));
     }
 
     @Test
-    public void shouldBeAbleToExtractFieldGoalPercentageFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractFieldGoalsMadeFromSeasonsTotalsPage() throws Exception {
+        fieldGoalsMadeValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(fieldGoalsMadeValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getFieldGoalsMade())));
     }
 
     @Test
-    public void shouldBeAbleToExtractThreePointersAttemptedFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractFieldGoalPercentageFromSeasonsTotalsPage() throws Exception {
+        fieldGoalPercentageValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(fieldGoalPercentageValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getFieldGoalPercentage())));
     }
 
     @Test
-    public void shouldBeAbleToExtractThreePointersMadeFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractThreePointersAttemptedFromSeasonsTotalsPage() throws Exception {
+        threePointersAttemptedValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(threePointersAttemptedValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getThreePointersAttempted())));
     }
 
     @Test
-    public void shouldBeAbleToExtractThreePointPercentageFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractThreePointersMadeFromSeasonsTotalsPage() throws Exception {
+        threePointersMadeValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(threePointersMadeValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getThreePointersMade())));
     }
 
     @Test
-    public void shouldBeAbleToExtractTwoPointersAttemptedFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractThreePointPercentageFromSeasonsTotalsPage() throws Exception {
+        threePointPercentageValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(threePointPercentageValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getThreePointPercentage())));
     }
 
     @Test
-    public void shouldBeAbleToExtractTwoPointersMadeFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractTwoPointersAttemptedFromSeasonsTotalsPage() throws Exception {
+        twoPointersAttemptedValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(twoPointersAttemptedValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getTwoPointersAttempted())));
     }
 
     @Test
-    public void shouldBeAbleToExtractTwoPointersPercentageFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractTwoPointersMadeFromSeasonsTotalsPage() throws Exception {
+        twoPointersMadeValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(twoPointersMadeValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getTwoPointersMade())));
     }
 
     @Test
-    public void shouldBeAbleToExtractEffectiveFieldGoalPercentageFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractTwoPointersPercentageFromSeasonsTotalsPage() throws Exception {
+        twoPointerPercentageValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(twoPointerPercentageValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getTwoPointPercentage())));
     }
 
     @Test
-    public void shouldBeAbleToExtractFreeThrowsAttemptedFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractEffectiveFieldGoalPercentageFromSeasonsTotalsPage() throws Exception {
+        effectiveFieldGoalPercentageValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(effectiveFieldGoalPercentageValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getEffectiveFieldGoalPercentage())));
     }
 
     @Test
-    public void shouldBeAbleToExtractFreeThrowsMadeFromSeasonsTotalsPage() {
-
-    }
-    @Test
-    public void shouldBeAbleToExtractFreeThrowPercentageFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractFreeThrowsAttemptedFromSeasonsTotalsPage() throws Exception {
+        freeThrowsAttemptedValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(freeThrowsAttemptedValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getFreeThrowsAttempted())));
     }
 
     @Test
-    public void shouldBeAbleToExtractOffensiveReboundsFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractFreeThrowsMadeFromSeasonsTotalsPage() throws Exception {
+        freeThrowsMadeValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(freeThrowsMadeValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getFreeThrowsMade())));
     }
 
     @Test
-    public void shouldBeAbleToExtractDefensiveReboundsFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractFreeThrowPercentageFromSeasonsTotalsPage() throws Exception {
+        freeThrowsPercentageValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(freeThrowsPercentageValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getFreeThrowPercentage())));
     }
 
     @Test
-    public void shouldBeAbleToExtractTotalReboundsFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractOffensiveReboundsFromSeasonsTotalsPage() throws Exception {
+        offensiveReboundsValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(offensiveReboundsValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getOffensiveRebounds())));
     }
 
     @Test
-    public void shouldBeAbleToExtractAssistsFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractDefensiveReboundsFromSeasonsTotalsPage() throws Exception {
+        defensiveReboundsValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(defensiveReboundsValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getDefensiveRebounds())));
     }
 
     @Test
-    public void shouldBeAbleToExtractStealsFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractTotalReboundsFromSeasonsTotalsPage() throws Exception {
+        totalReboundsValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(totalReboundsValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getTotalRebounds())));
     }
 
     @Test
-    public void shouldBeAbleToExtractBlocksFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractAssistsFromSeasonsTotalsPage() throws Exception {
+        assistsValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(assistsValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getAssists())));
     }
 
     @Test
-    public void shouldBeAbleToExtractTurnoversFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractStealsFromSeasonsTotalsPage() throws Exception {
+        stealsValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(stealsValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getSteals())));
     }
 
     @Test
-    public void shouldBeAbleToExtractPersonalFoulsFromSeasonsTotalsPage() {
-
+    public void shouldBeAbleToExtractBlocksFromSeasonsTotalsPage() throws Exception {
+        blocksValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(blocksValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getBlocks())));
     }
 
     @Test
-    public void shouldBeAbleToExtractPointsFromSeasonsTotalsPage() {
+    public void shouldBeAbleToExtractTurnoversFromSeasonsTotalsPage() throws Exception {
+        turnoversValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(turnoversValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getTurnovers())));
+    }
 
+    @Test
+    public void shouldBeAbleToExtractPersonalFoulsFromSeasonsTotalsPage() throws Exception {
+        personalFoulsValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(personalFoulsValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getPersonalFouls())));
+    }
+
+    @Test
+    public void shouldBeAbleToExtractPointsFromSeasonsTotalsPage() throws Exception {
+        pointsValueExtractor.setInputStringToSearch(quincyAcySeasonTotals);
+        assertThat(pointsValueExtractor.getValue(), is(equalTo((Object) expectedQuincyAcyModel.getPoints())));
     }
 }
