b38e5932db4178e9e9bafa6f170a2d56d7783416
Brian Tanabe <btanabe2@gmail.com>
2015-12-06 22:38:37 -0600

    Threaded the method which is responsible for parsing and setting a value in a Model.  This reduced the RecordCollector tests' runtime ~44%.  There is still room for improvement.

--- src/main/java/com/btanabe/fsdu/models/factories/ModelFactory.java
+++ src/main/java/com/btanabe/fsdu/models/factories/ModelFactory.java
@@ -20,13 +20,18 @@ public class ModelFactory<OutputClazz> extends AbstractModelFactory {
     @Override
     public Object createObject() throws Exception {
         OutputClazz model = (OutputClazz) createInstance();
+        setterMethodToValueMap.keySet().parallelStream().forEach(setterMethodName -> getValueAndStoreInModelUsingTheParameterizedSetterMethodName(model, setterMethodName));
+        return model;
+    }
 
-        for (String setterMethodName : setterMethodToValueMap.keySet()) {
+    // TODO I KNOW THERE'S A MORE ELEGANT WAY TO HANDLE METHODS WHICH THROW EXCEPTIONS USED IN LAMBDA FUNCTIONS:
+    private void getValueAndStoreInModelUsingTheParameterizedSetterMethodName(OutputClazz model, String setterMethodName) {
+        try {
             Object value = setterMethodToValueMap.get(setterMethodName).getValue();
             ClassUtils.getMethod(outputClazzClass, setterMethodName, value.getClass()).invoke(model, value);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
         }
-
-        return model;
     }
 
     @Override
