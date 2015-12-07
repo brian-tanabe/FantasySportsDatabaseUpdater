6fc4887bdf9fbaf24146ac89c82a5793e9ef53ad
Brian Tanabe <btanabe2@gmail.com>
2015-12-06 20:42:22 -0600

    Refactored test class names and killed an unnecessary comment in the value extractor spring configuration file.

--- src/test/java/com/btanabe/fsdu/test/unit/parsers/record/parser/EspnProjectionsPageRecordParserTests.java
+++ src/test/java/com/btanabe/fsdu/test/unit/parsers/record/parser/EspnProjectionsPageRecordParserTests.java
@@ -20,7 +20,7 @@ import static org.junit.Assert.assertThat;
  */
 @ContextConfiguration("classpath:spring-configuration/unit-testing-configuration.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
-public class EspnRecordParserTests {
+public class EspnProjectionsPageRecordParserTests {
 
     @Autowired
     @Qualifier("espnNflPlayerProjectionPagePaginatedRecordParser")
