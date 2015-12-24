fd2f110133a35e8a62e6b533dc6126fbe30f6dca
Brian Tanabe <btanabe2@gmail.com>
2015-12-24 17:36:39 -0600

    adding more test files and applicaiton files

--- src/main/java/com/btanabe/fsdu/application/Application.java
+++ src/main/java/com/btanabe/fsdu/application/Application.java
@@ -1,7 +1,16 @@
 package com.btanabe.fsdu.application;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by Brian on 12/23/15.
  */
 public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-configuration/application-configuration.xml");
+
+        new EspnPlayerOwnershipScraper().printPlayersOwnedByTheParameterizedTeam("Ty Fighter");
+    }
 }
--- src/main/java/com/btanabe/fsdu/application/EspnPlayerOwnershipScraper.java
+++ src/main/java/com/btanabe/fsdu/application/EspnPlayerOwnershipScraper.java
@@ -1,7 +1,25 @@
 package com.btanabe.fsdu.application;
 
+import com.btanabe.fsdu.collectors.RecordCollector;
+import com.btanabe.fsdu.models.EspnFantasyLeaguePlayerOwnershipModel;
+import com.btanabe.fsdu.web.WebRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.Set;
+
 /**
  * Created by Brian on 12/23/15.
  */
+
 public class EspnPlayerOwnershipScraper {
+
+    @Autowired
+    @Qualifier("espnNbaPlayerRaterPageRecordCollector")
+    private RecordCollector nbaPlayerRaterPageRecordCollector;
+
+    public void printPlayersOwnedByTheParameterizedTeam(String espnTeamName) {
+        Set<EspnFantasyLeaguePlayerOwnershipModel> allPlayersOwnershipInformation = (Set<EspnFantasyLeaguePlayerOwnershipModel>) nbaPlayerRaterPageRecordCollector.apply("http://games.espn.go.com/fba/playerrater?leagueId=233928", new WebRequest());
+        allPlayersOwnershipInformation.stream().filter(player -> player.getEspnFantasyLeagueId().equals(espnTeamName)).forEach(System.out::println);
+    }
 }
